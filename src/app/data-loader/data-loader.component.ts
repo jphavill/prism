b06c2c09bb4d2f3efb534c158cd41d0e4685c499
Jason Havill <jason@Jasons-MacBook-Pro.local>
2025-03-08 19:31:38 -0400

    Initialize angular project and add test data

---
+++ src/app/data-loader/data-loader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-loader',
+  imports: [],
+  templateUrl: './data-loader.component.html',
+  styleUrl: './data-loader.component.scss'
+})
+export class DataLoaderComponent {
+
+}
